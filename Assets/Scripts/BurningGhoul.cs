96336c34611581083ff015123aae27ac76f8f01d
sema <sam.semasen@gmail.com>
2020-05-10 13:28:21 +0300

    Enemies Created
    Basic enemy animations and scripts

---
+++ Assets/Scripts/BurningGhoul.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningGhoul : MonoBehaviour
+{
+    public float speed;
+    private bool movingRight = true;
+    
+    void Start()
+    {
+        //Ignore the collisions between layer 0 (default) and layer 8 (Room) 
+        Physics2D.IgnoreLayerCollision(0, 8, true);
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(movingRight== true) {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            movingRight = false;
+        } else {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject[] whichRow; // row1 = 5f, row2 =-5f ,row3 =-15f, row4=-25f;
+    float randX;
+    
+    
+    Vector2 whereToSpawn;
+    public float spawnRate = 10f;
+    float nextSpawn = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(0f, 60f);
+            int rand = Random.Range(0,4);
+            whereToSpawn = new Vector2(randX, whichRow[rand].transform.position.y);
+            int randEnemy = Random.Range(0, 2);
+            Instantiate(enemies[randEnemy], whereToSpawn, Quaternion.identity);
+
+
+        }
+    }
+}
